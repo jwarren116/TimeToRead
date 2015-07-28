c41f0bbd5ee198374fca2471991e0ae29f955e3f
James Warren <jwarren116@gmail.com>
2015-07-28 11:33:44 -0700

    Build out script to print estimated time to read text from JSON's  key returned by API

--- timetoread.py
+++ timetoread.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
 from math import ceil
+import requests
 
-# your code here :)
+# make GET request to API and bind response to 'resp'
+resp = requests.get('http://www.expertise.com/api/v1.0/jobs/text')
+
+# bind value of JSON object's 'text' to list 'text' split on whitespace
+text = resp.json()['text'].split()
+
+# print estimated time to read text, assuming average of 200wpm
+# text/200.0 returns float, which is rounded up and converted to an int
+print "about {time} minutes".format(time=int(ceil(len(text)/200.0)))
